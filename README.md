3c32b3057d909784b265dc7947b4bde25b2e4ba3
Daynssjal <danisova.dadka@gmail.com>
2019-03-04 10:31:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spot-the-difference-game
+A web-based game built with vanilla JavaScript, CSS, and Html. 
---
+++ js/list.js
@@ -0,0 +1,114 @@
+images = [
+  {
+    img: ["img/01_A.jpg", "img/01_B.jpg"],
+
+    actual: [
+      "Shoe Colour",
+      "Sign Text",
+      "Security Camera",
+      "Ceiling Windows",
+      "Background Wall Colour"
+    ],
+
+    fake: [
+      "Baseball Cap",
+      "Overhead Lights",
+      "T-Shirt Colour",
+      "Exit Sign",
+      "Socks",
+      "Wrist Watch",
+      "Backpack",
+      "Elevator Rail",
+      "Belt",
+      "Elevator Footlights"
+    ]
+  },
+
+  {
+    img: ["img/02_A.jpg", "img/02_B.jpg"],
+
+    actual: [
+      "Road Sign",
+      "Background Houses",
+      "Boulder in front of the House",
+      "Roof Windows",
+      "Road"
+    ],
+
+    fake: [
+      "Power Cables",
+      "Boat Color",
+      "Farm Animals",
+      "Chimneys",
+      "Hitch Hiker",
+      "Hibernating Birds",
+      "Man By Window",
+      "Doorway Shape",
+      "Dumpster Color",
+      "Street Lights"
+    ]
+  },
+
+  {
+    img: ["img/03_A.jpg", "img/03_B.jpg"],
+
+    actual: ["Flag", "Background Mountains", "Bridge", "Boat", "Chimney"],
+
+    fake: [
+      "Power Cables",
+      "Bagpiper",
+      "Nessie",
+      "Seagulls",
+      "Haggis",
+      "Hibernating Birds",
+      "Castle Window",
+      "Deer",
+      "Torches",
+      "Lifeguard"
+    ]
+  },
+
+  {
+    img: ["img/04_A.jpg", "img/04_B.jpg"],
+
+    actual: ["Flag", "Missing Man", "Windows", "Billboard", "Lamp Post"],
+
+    fake: [
+      "Power Cables",
+      "Traffic Lights",
+      "Security Camera",
+      "Scooter",
+      "Tourist Bus",
+      "Statue",
+      "Street Performer",
+      "Dog",
+      "Roadworks Sign",
+      "Policeman"
+    ]
+  },
+
+  {
+    img: ["img/05_A.jpg", "img/05_B.jpg"],
+
+    actual: [
+      "Speed-Limit Sign",
+      "Incoming Car",
+      "Power Lines",
+      "Village Sign",
+      "Missing House"
+    ],
+
+    fake: [
+      "Fuel Station",
+      "Jogger",
+      "Security Camera",
+      "Farmyard Animals",
+      "Patrol Car",
+      "Zebra Crossing",
+      "Railway Line",
+      "Dog",
+      "Roadworks Sign",
+      "Policeman"
+    ]
+  }
+];
